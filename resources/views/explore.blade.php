194b67dcb841665d4415b95e6c86f63a6100a1f0
jainebudke <jainebudke@hotmail.com>
2018-08-31 13:51:50 -0300

    alteracoes no servidor

--- resources/views/explore.blade.php
+++ resources/views/explore.blade.php
@@ -332,10 +332,10 @@
                                                 @endif
         
                                                 @if( $saved_obj == false )
-                                                    <button class="button-save" type="submit"><i class="far fa-star" style="color: #4d4d4d; font-size: 1.5em"></i></button>
+                                                    <button class="button-save" type="submit"><i class="fa fa-star" style="color: #4d4d4d; font-size: 1.5em"></i></button>
                                                     <!--button class="button-save button-active"></button>-->
                                                 @else
-                                                    <button class="button-save" type="submit"><i class="fas fa-star" style="color: #4d4d4d; font-size: 1.5em"></i></button>
+                                                    <button class="button-save" type="submit"><i class="fa fa-star" style="color: #4d4d4d; font-size: 1.5em"></i></button>
 
                                                     <!--<button class="button-save button-deactivated" type="button"></button>-->
                                                 @endif
